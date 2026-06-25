60bd041b715b27fc13c63409c98b77df5bb89e9a
NeonAngelThreads <pienadev495@gmail.com>
2026-06-25 20:19:01 +0800

    Initial commit

---
+++ src/main/java/org/microsoft/qintelipass/QZhipassApplication.java
@@ -0,0 +1,11 @@
+package org.microsoft.qintelipass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QZhipassApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(QZhipassApplication.class, args);
+    }
+}
---
+++ src/test/java/org/microsoft/qintelipass/QZhipassApplicationTests.java
@@ -0,0 +1,13 @@
+package org.microsoft.qintelipass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QZhipassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
